0ae4da741f2c0d6e01f7894405424c9971eabc99
dinar <dinargaizetdinov@yandex.ru>
2025-10-05 14:38:25 +0500

    dinar second commit - change print

--- gayzetdinov_dinar.ру.py
+++ gayzetdinov_dinar.ру.py
@@ -2,6 +2,6 @@ def main():
     print("Гайзетдинов Динар")
     print("ИВТ-ИВС-202Б")
     print("05.10.2025")
-    print("The show must go on")
+    print("You are the sunshine of my life ")
     if __name__ == "__main__":
         main()
